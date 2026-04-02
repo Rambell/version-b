27ad411464ad5e7b8dd09cc1a45e551caed77d4a
matias <mat.ramirez@duocuc.cl>
2026-04-02 02:15:10 -0300

    Desarrollo seccion hero

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,18 @@
 $(document).ready(function() {
     console.log('ADIPA JS loaded');
 });
+
+$(document).ready(function () {
+
+    // Cerrar announcement bar
+    $('#close-announcement').on('click', function () {
+        $('#announcement-bar').slideUp(200);
+    });
+
+    // Quick searches hero
+    $('.hero__quick-btn').on('click', function () {
+        const term = $(this).data('term');
+        $('#hero-search').val(term);
+    });
+
+});
