c5a58be6dc5cc329ffdc642447641c84089ee33d
matias <mat.ramirez@duocuc.cl>
2026-04-02 01:23:49 -0300

    Se desarrolla header

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,52 @@
+<nav class="navbar">
+    <div class="navbar__container">
+        <div class="navbar__left">
+            <div class="navbar__whatsapp">
+                <img src="https://storage.googleapis.com/statics-files-adipa-cl/dist_compress/dist/img/icons/icons-whatsapp.svg"
+                     alt="WhatsApp" class="navbar__whatsapp-icon">
+                <span class="navbar__separator">|</span>
+            </div>
+
+            <ul class="navbar__links">
+                <li class="navbar__item navbar__item--dropdown">
+                    <a href="#" class="navbar__link">Descubre ADIPA
+                        <svg width="12" height="12" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
+                        </svg>
+                    </a>
+                </li>
+                <li class="navbar__item navbar__item--dropdown">
+                    <a href="#" class="navbar__link">Recursos
+                        <svg width="12" height="12" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
+                        </svg>
+                    </a>
+                </li>
+                <li class="navbar__item">
+                    <a href="#" class="navbar__link navbar__link--badge">
+                        <span class="navbar__badge navbar__badge--gratis">GRATIS</span>
+                        Seminarios
+                    </a>
+                </li>
+                <li class="navbar__item">
+                    <a href="#" class="navbar__link navbar__link--badge">
+                        <span class="navbar__badge navbar__badge--nuevo">NUEVO</span>
+                        Congreso
+                    </a>
+                </li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Especializaciones</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Acreditaciones</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Sesiones Magistrales</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link">Diplomados</a></li>
+                <li class="navbar__item"><a href="#" class="navbar__link navbar__link--bold">Cursos</a></li>
+            </ul>
+        </div>
+
+        <a href="#" class="navbar__blacksale">
+            <img src="https://adipa.cl/content/uploads/2026/03/black-sale-icono-mini-banner-1.webp"
+                 alt="Black Sale" class="navbar__blacksale-icon">
+            <span class="navbar__blacksale-divider"></span>
+            <span class="navbar__blacksale-text">Black Sale Oficial</span>
+        </a>
+    </div>
+</nav>
